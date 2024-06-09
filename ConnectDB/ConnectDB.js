3b566987c32f9881db660609c51445bdfe186e34
mufaisa <mfkamalimdia@gmail.com>
2024-06-10 04:17:44 +0530

    Changed Connection String

--- ConnectDB/ConnectDB.js
+++ ConnectDB/ConnectDB.js
@@ -1,13 +1,9 @@
 import mongoose from "mongoose";
-import { EmployeeModel } from "../Models/EmployeeModel.js";
 
 export const ConnectDB = () => {
-    mongoose.connect(`mongodb://127.0.0.1:27017/Portfolio`)
+    mongoose.connect(`mongodb+srv://mfk:test123@portfolio.lsuj3bc.mongodb.net/Portfolio?retryWrites=true&w=majority&appName=Portfolio`)
         .then(res => {
             console.log(`Connected to DB`)
-            EmployeeModel.find()
-                .then(employee => console.log(employee[0].Name))
-                .catch(err => console.log(err))
         })
         .catch(er => console.log(er))
 }
